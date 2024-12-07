fc1386339a6413906309846f2f4e491fd825df1e
elNoisY <159972832+elNoisY@users.noreply.github.com>
2024-12-07 14:26:32 -0500

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     function initMap() {
         const map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 14,
-            center: {lat: -12.046374, lng: -77.042793},
+            zoom: 8,
+            center: {lat: -34.397, lng: 150.644},
         });
 
         const restaurantes = [
